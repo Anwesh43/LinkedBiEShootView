a49afcb8c847a57c50c4cd6e7242fcbffa3ebe8a
Anwesh43 <anweshthecool0@gmail.com>
2019-01-22 10:41:57 +0530

    adding BiEShoot class

--- bieshootview/src/main/java/com/anwesh/uiprojects/bieshootview/BiEShootView.kt
+++ bieshootview/src/main/java/com/anwesh/uiprojects/bieshootview/BiEShootView.kt
@@ -162,7 +162,31 @@ class BiEShootView(val ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
         }
+    }
+
+    data class BiEShoot(var i : Int) {
+        private val root : BESNode = BESNode(0)
+        private var curr : BESNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+
     }
 }
